52af449257a628233a43fbfc3369d9e1df85b6bb
Ethan Biggs <ethanscott402@gmail.com>
2025-02-21 17:51:48 -0700

    First wave of changes - movie list, edit, delete

--- Mission06_Biggs/Controllers/HomeController.cs
+++ Mission06_Biggs/Controllers/HomeController.cs
@@ -36,5 +36,51 @@ namespace Mission06_Biggs.Controllers
             _context.SaveChanges();
             return View("Confirmation", movie);
         }
+
+        public IActionResult MovieList()
+        {
+            var Movies = _context.Movies
+                .OrderBy(x => x.Title)
+                .ToList();
+
+            return View(Movies);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var movie = _context.Movies
+                .Single(x => x.movieId == id);
+
+            return View("AddMovie", movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(MovieModel movie)
+        {
+            _context.Update(movie);
+            _context.SaveChanges();
+
+            return View("Confirmation", movie);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var movie = _context.Movies
+                .Single(x => x.movieId == id);
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(MovieModel movie)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieList");
+        }
+     
     }
 }
--- Mission06_Biggs/Models/MovieModel.cs
+++ Mission06_Biggs/Models/MovieModel.cs
@@ -7,14 +7,14 @@ namespace Mission06_Biggs.Models
 
         [Key]
         [Required]
-        public int movieId { get; set; }
-        public string Category { get; set; }
-        public string Title { get; set; }
-        public int Year { get; set; }
-        public string Director { get; set; }
-        public string Rating { get; set; }
+        public int? movieId { get; set; }
+        public string? Category { get; set; }
+        public string? Title { get; set; }
+        public int? Year { get; set; }
+        public string? Director { get; set; }
+        public string? Rating { get; set; }
         public bool? Edited { get; set; }
-        public string LentTo { get; set; } = string.Empty;
-        public string Notes { get; set; } = string.Empty;
+        public string? LentTo { get; set; } = string.Empty;
+        public string? Notes { get; set; } = string.Empty;
     }
 }
